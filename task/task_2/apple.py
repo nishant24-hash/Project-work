6f11d9505a6fd0095ff68ca468825e8c7fc9f028
Nishant Kumar Shah <snishant24@tbc.edu.np>
2026-01-19 01:03:02 +0545

    apple added successfully

---
+++ task/task_2/apple.py
@@ -0,0 +1,11 @@
+# Python program to add two numbers
+
+# Taking input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Adding the two numbers
+sum = num1 + num2
+
+# Displaying the result
+print("The sum of", num1, "and", num2, "is:", sum)
